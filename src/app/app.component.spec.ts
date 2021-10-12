562727d16b9d1a48575e94d2cba7470668b32c8b
pthoang3 <thuyminx01042001@gmail.com>
2021-10-12 12:44:12 -0400

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-angular'`, () => {
+  it(`should have as title 'Thuy Hoang | Portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-angular');
+    expect(app.title).toEqual('Thuy Hoang | Portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './containers/header/header.component';
+import { NavbarComponent } from './containers/header/navbar/navbar.component';
+import { HomeComponent } from './containers/header/home/home.component';
+import { AboutMeComponent } from './containers/about-me/about-me.component';
+import { WorksComponent } from './containers/works/works.component';
+import { FooterComponent } from './containers/footer/footer.component';
+import { ContactComponent } from './containers/contact/contact.component';
+import { SkillsComponent } from './containers/skills/skills.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutMeComponent,
+    WorksComponent,
+    FooterComponent,
+    ContactComponent,
+    SkillsComponent,
   ],
   imports: [
     BrowserModule
